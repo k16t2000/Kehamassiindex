1410570a326a8599663dbd6bf83d7060e2997250
k16t2000 <k16t2000@mail.ru>
2020-09-17 13:21:11 +0300

    massiindex

--- server.js
+++ server.js
@@ -11,13 +11,30 @@ app.get("/",function(request,response){
 app.post('/',function(request,response){
     console.log(request.body);//zapros body
     let num1=Number(request.body.num1);
-    let num2=Number(request.body.num2);
-    let result=num1+num2;
-    console.log(`${num1}+${num2}=${result}`);
-    response.send(`${num1}+${num2}=${result}`);//result 10
+    let num2=Number(request.body.num2/100);
+    let result=(num1/(num2*num2)).toFixed(1);//konvert posle zapatoj
+    
+    let x= `Kehamassiindeks arvutamise valem: `;
+        console.log(`${num1}/${num2}*${num2}=${result}`);//pokazivaet v konsole
+    
+
+    if(result<19){      
+        response.send(`${x}${num1}/${num2}*${num2}=${result} - Alakaal`);//pokazivaet v brauzere
+        console.log('Alakaal');  
+    }else if(result>=19 && result<=24.9){
+        response.send(`${x}${num1}/${num2}*${num2}=${result} - Normaalkaal`);//pokazivaet v brauzere
+        console.log('Normaalkaal');
+    }else if(result>=25 && result<=29.9){
+        response.send(`${x}${num1}/${num2}*${num2}=${result} - Ülekaal`);//pokazivaet v brauzere
+        console.log('Ülekaal');
+    }else{
+        response.send(`${x}${num1}/${num2}*${num2}=${result} - Rasvumine`);//pokazivaet v brauzere
+        console.log('Rasvumine');
+    }
 });
 
-app.listen(3000, function(){//nomer porta 3000, error funkzija
-    console.log('Server is running on Port 3000');//call back funktsija
+
+app.listen(4000, function(){//nomer porta 4000, error funkzija
+    console.log('Server is running on Port 4000');//call back funktsija
     
 });
